9940a672a1434c3e6466eeb84cd3bfe58c385e65
jiwatmikapratama <jiwatmikapratama@gmail.com>
2023-07-27 16:37:11 +0800

    middleware

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,40 @@ use Symfony\Component\HttpKernel\Profiler\Profile;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Product Controller
+Route::get('/product', [ProductController::class, 'index'])->name('index.product');
+
+Route::middleware(['is_admin'])->group(function () {
+    Route::get('/product/create', [ProductController::class, 'create'])->name('create.product');
+    Route::post('/product/create', [ProductController::class, 'store'])->name('store.product');
+    Route::get('/product/edit/{product}', [ProductController::class, 'edit'])->name('edit.product');
+    Route::patch('/product/edit/{product}', [ProductController::class, 'update'])->name('update.product');
+    Route::post('/order/{order}/confirm', [OrderController::class, 'confirm_payment'])->name('confirm.payment');
+    Route::delete('/product/delete/{product}', [ProductController::class, 'destroy'])->name('delete.product');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/product/{product}', [ProductController::class, 'show'])->name('show.product');
+
+
+    // Cart Controller
+    Route::post('/cart/{product}', [CartController::class, 'add_to_cart'])->name('add_to_cart');
+    Route::get('/cart', [CartController::class, 'show_cart'])->name('show.cart');
+    Route::patch('/cart/update/{cart}', [CartController::class, 'update_cart'])->name('update.cart');
+    Route::delete('/cart/delete/{cart}', [CartController::class, 'destroy_cart'])->name('delete.cart');
+
+
+
+    // Order Controller
+    Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+    Route::get('/order', [OrderController::class, 'index'])->name('index.order');
+    Route::get('/order/{order}', [OrderController::class, 'show_order'])->name('show.order');
+    Route::post('/order/{order}/pay', [OrderController::class, 'submit_payment_receipt'])->name('submit_payment_receipt');
+
+    //Use Controller
+    Route::get('users/profile', [ProfileController::class, 'show'])->name('show.user');
+    Route::post('users/profile', [ProfileController::class, 'update'])->name('update.user');
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,30 +60,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Product Controller
-Route::get('/product', [ProductController::class, 'index'])->name('index.product');
-Route::get('/product/create', [ProductController::class, 'create'])->name('create.product');
-Route::post('/product/create', [ProductController::class, 'store'])->name('store.product');
-Route::get('/product/{product}', [ProductController::class, 'show'])->name('show.product');
-Route::get('/product/edit/{product}', [ProductController::class, 'edit'])->name('edit.product');
-Route::patch('/product/edit/{product}', [ProductController::class, 'update'])->name('update.product');
-Route::delete('/product/delete/{product}', [ProductController::class, 'destroy'])->name('delete.product');
-
-// Cart Controller
-Route::post('/cart/{product}', [CartController::class, 'add_to_cart'])->name('add_to_cart');
-Route::get('/cart', [CartController::class, 'show_cart'])->name('show.cart');
-Route::patch('/cart/update/{cart}', [CartController::class, 'update_cart'])->name('update.cart');
-Route::delete('/cart/delete/{cart}', [CartController::class, 'destroy_cart'])->name('delete.cart');
-
-
-// Order Controller
-Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
-Route::get('/order', [OrderController::class, 'index'])->name('index.order');
-Route::get('/order/{order}', [OrderController::class, 'show_order'])->name('show.order');
-Route::post('/order/{order}/pay', [OrderController::class, 'submit_payment_receipt'])->name('submit_payment_receipt');
-Route::post('/order/{order}/confirm', [OrderController::class, 'confirm_payment'])->name('confirm.payment');
-
-//Use Controller
-Route::get('users/profile', [ProfileController::class, 'show'])->name('show.user');
-Route::post('users/profile', [ProfileController::class, 'update'])->name('update.user');
